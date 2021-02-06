222f64c61b7b50c5d11bc93fb703738b59100af1
Campbell <KidzCode42@gmail.com>
2021-02-06 12:01:27 -0600

    Worked on CreateVector. We got the cone oriented the right way, and the cylinder  that is going to be the vector is the right size, postion, and orientation as well.

--- Assets/GameLogic.cs
+++ Assets/GameLogic.cs
@@ -19,7 +19,7 @@ public class GameLogic : MonoBehaviour
 		if (ableToCreateVector)
 		{
 			ableToCreateVector = false;
-			CreateVector(7, 7, 8);
+			CreateVector(3, 2, 1);
 		}
 	}
 
@@ -31,11 +31,25 @@ public class GameLogic : MonoBehaviour
 		}
 	}
 
-	void CreateVector(int x, int y, int z)
+	void CreateVector(double endX, double endY, double endZ, double startX = 0, double startY = 0, double startZ = 0)
 	{
-		Vector3 position = new Vector3(x, y, z);
-		// We are going to assume the beginning of the vector is the origin (0, 0, 0).
-		Instantiate(Cone, position, Quaternion.identity);
+		double deltaX = endX - startX;
+		double deltaY = endY - startY;
+		double deltaZ = endZ - startZ;
+		double halfPosX = startX + deltaX / 2;
+		double halfPosY = startY + deltaY / 2;
+		double halfPosZ = startZ + deltaZ / 2;
+		Vector3 vectorDirection = new Vector3((float)deltaX, (float)deltaY, (float)deltaZ);
+		Vector3 position = new Vector3((float)endX, (float)endY, (float)endZ);
+		double length = Math.Sqrt(Math.Pow(deltaX, 2) + Math.Pow(deltaY, 2) + Math.Pow(deltaZ, 2));
+
+		GameObject cone = Instantiate(Cone, position, Quaternion.LookRotation(vectorDirection));
 		//Debug.Log("Creating Vector");
+		GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+		cylinder.transform.localPosition = new Vector3((float)halfPosX, (float)halfPosY, (float)halfPosZ);
+		Quaternion lookRotation = Quaternion.LookRotation(vectorDirection);
+		cylinder.transform.localRotation = lookRotation;
+		cylinder.transform.Rotate(Vector3.right, 90);  // Because the cylinder's default orientation appears to be completely wrong!
+		cylinder.transform.localScale = new Vector3(0.5f, (float)length / 2, 0.5f); // For some reason the cylinder scales twice as long as a cube
 	}
 }
